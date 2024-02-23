89372bfb33174313197f04f75173ac78e3358019
Barbara Reis <118932638+brareisds@users.noreply.github.com>
2024-02-23 15:10:48 -0300

    classificador knn

--- knn_classifier.py
+++ knn_classifier.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from tqdm import tqdm  # Importa a função tqdm
+from tqdm import tqdm 
 
 file = 'dados_hist.csv'
 
@@ -30,7 +30,7 @@ max_values = X_treino.max()
 X_treino_normalizado = (X_treino - min_values) / (max_values - min_values)
 X_teste_normalizado = (X_teste - min_values) / (max_values - min_values)
 
-# Função para calcular a distância euclidiana entre dois pontos
+# Calcula a distância euclidiana entre dois pontos
 def euclidean_distance(x1, x2):
     dist = np.sqrt(np.sum((x1 - x2) ** 2))
     return dist
@@ -38,8 +38,6 @@ def euclidean_distance(x1, x2):
 def knn_predict(X_train, y_train, x_test, k=3):
     distances = []
     for _, x_train in X_train.iterrows():
-        #print(x_train)
-        # print(f'x_test = {x_test}')
         distance = euclidean_distance(x_test, x_train)
         distances.append(distance)
 
@@ -51,43 +49,31 @@ def knn_predict(X_train, y_train, x_test, k=3):
 k = 3
 correct_predictions = 0
 
-# Fazendo a previsão para cada linha de teste
+# Faz a previsão para cada linha de teste
 predictions = []
 for index, row in tqdm(X_teste_normalizado.iterrows(), total=len(X_teste_normalizado)):
     prediction = knn_predict(X_treino_normalizado, y_treino, row, k)
     true_class = y_teste.loc[index]
+    # Compara a classe prevista com a classe verdadeira
     if prediction == true_class:
         correct_predictions += 1
-    #print(f'prediction: {prediction} | classe verdadeira:', true_class)
     predictions.append(prediction)
 
 
-# Imprimindo as previsões
-# for i, prediction in enumerate(predictions):
-#     print(f"Previsão para a linha {i}: {prediction}")
-
-print(len(predictions))
-print(f'correct predictions: {correct_predictions}')
-
 accuracy = correct_predictions / len(predictions)
-print(f'Acurácia: {accuracy}')
 accuracy_percent = accuracy * 100
 print(f'Acurácia: {accuracy_percent:.2f}%')
 
-
-# Convertendo a série para uma lista
+# Converte a série para uma lista
 y_true = y_teste.tolist()
-
-# Inicializando a matriz de confusão
 confusion_matrix = [[0, 0], [0, 0]]
 
-# Preenchendo a matriz de confusão
+# Preenche a matriz de confusão
 for i in range(len(y_true)):
     true_class = y_true[i]
     pred_class = predictions[i]
     confusion_matrix[int(true_class)][int(pred_class)] += 1
 
-# Imprimindo a matriz de confusão formatada
 print("Matriz de Confusão:")
 print("           Empty     Occupied ")
 print("---------------------------------------")
